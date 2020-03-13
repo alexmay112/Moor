e3f959ba828f1945f9d152a46c8dadc098df3a12
O.M <a.majstrenko@everad.com>
2020-03-13 18:38:00 +0200

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import './helpers/postDate';
-import scrollSmooth from './helpers/smooth-scroll.js';
-import example from './modules/example.js';
+// import scrollSmooth from './helpers/smooth-scroll.js';
+// import example from './modules/example.js';
 
 function main () {
-  scrollSmooth();
-  example();
+  // scrollSmooth();
+  // example();
 }
 
 main();
